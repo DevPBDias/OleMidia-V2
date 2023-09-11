7c62373e2930f68c6a6a7f821720b2a32e0c796e
DevPBDias <devpbdias@gmail.com>
2023-09-11 15:43:18 -0300

    feat: header component and styles added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,32 @@
+import logo from '../../assets/OleLogo.svg';
+import instagramIcon from '../../assets/Instagram.svg';
+import twitterIcon from '../../assets/Twitter.svg';
+import { HeaderBar, NavBar, OleLogo,
+  ContainerNavIcons, Link, ContainerIcons, Icons } from './styles';
+
 function Header() {
   return (
-    <div>Header</div>
+    <HeaderBar>
+      <OleLogo alt="oleHeader" src={ logo } />
+      <ContainerNavIcons className="headerLinks">
+        <NavBar>
+          <Link href="#home">Home</Link>
+          <Link href="#who">Quem somos</Link>
+          <Link href="#services">Serviços</Link>
+          <Link href="#client">Clientes</Link>
+          <Link href="#portfolio">Portfólio</Link>
+          <Link href="#contact">Contato</Link>
+        </NavBar>
+        <ContainerIcons className="headerIcons">
+          <a href="https://www.instagram.com/olemidia/">
+            <Icons alt="instagram" src={ instagramIcon } className="instagramIcon" />
+          </a>
+          <a href="https://twitter.com/olemidia">
+            <Icons alt="instagram" src={ twitterIcon } className="instagramIcon" />
+          </a>
+        </ContainerIcons>
+      </ContainerNavIcons>
+    </HeaderBar>
   );
 }
 
---
+++ src/components/header/styles.tsx
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+export const HeaderBar = styled.header`
+    width: 100%;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+    justify-items: center;
+    align-items: center;
+    height: 7.75rem;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background: #FF6224;
+`;
+
+export const OleLogo = styled.img`
+    width: 4.75rem;
+    height: 3.375rem;
+    flex-shrink: 0;
+`;
+
+export const ContainerNavIcons = styled.section`
+    display: flex;
+    flex: row nowrap;
+    justify-content: space-between;
+    justify-items: center;
+    align-items: center;
+    gap: 3rem;
+    text-transform: uppercase;
+    padding: 1rem;
+`;
+
+export const NavBar = styled.nav`
+    display: flex;
+    flex: row;
+    justify-content: space-around;
+    justify-items: center;
+    align-items: center;
+    gap: 1.2rem;
+    padding: 1rem;
+`;
+
+export const Link = styled.a`
+    color: #EBE4DE;
+    font-family: 'Open Sans';
+    font-size: 1.125rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    text-decoration: none;
+`;
+
+export const ContainerIcons = styled.section`
+    display: flex;
+    flex: row;
+    justify-items: center;
+    align-items: center;
+    gap: 0.8rem;
+`;
+
+export const Icons = styled.img`
+    width: 1.99638rem;
+    height: 1.99638rem;
+    flex-shrink: 0;
+`;
